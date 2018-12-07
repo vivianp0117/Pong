32980443598e8a8191915afde5d4f2cb2ae55dc7
Vivian <vivian.phung17@gmail.com>
2018-12-07 10:19:29 -0500

    Uploaded Basic Pong Code

---
+++ Pong.java
@@ -0,0 +1,90 @@
+// practice task and homework
+// create one ball in a new program 2 rectangles and and create PONG ping pong!!!!
+
+// ball controlled by path need if statement if u bounce off the wall OR PADEL AND MORE IF BALL GOES TO ONE END OR ANOTHER 
+// Mouse Listener  -- Right Panel 
+// Key Listenr -- Left Panel
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Pong implements ActionListener, KeyListener, MouseMotionListener{
+	
+	//	Properties
+	JFrame frame;
+	PongPanel panel;
+	Timer timer;
+	
+	//	Methods
+	public void actionPerformed(ActionEvent evt){
+		if(evt.getSource() == timer){
+			panel.repaint();
+		}	
+	}
+	
+	//	KeyListener Methods
+
+	public void keyReleased(KeyEvent kevt){
+		
+		if(kevt.getKeyCode() == 38){
+			panel.blnUp = false;
+		}else if(kevt.getKeyCode() == 40){
+			panel.blnDown = false;
+		}
+			
+	}
+	
+	public void keyPressed(KeyEvent kevt){
+		if(kevt.getKeyCode() == 38){
+			panel.blnUp = true;
+		}else if(kevt.getKeyCode() == 40){
+			panel.blnDown = true;
+		}
+	}
+	
+	public void keyTyped(KeyEvent kevt){
+	}
+	
+	//	MouseMotion Listener
+		public void mouseMoved(MouseEvent mevt){
+			panel.intRect2Y = mevt.getY();
+		}
+		public void mouseDragged(MouseEvent mevt){
+		
+		
+		}
+	
+	
+	//	Constructor
+	public Pong(){
+	
+		frame = new JFrame ("Pong");
+		panel = new PongPanel();
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension (600,400));
+		panel.addMouseMotionListener(this);
+	
+	//	frame
+		frame.addKeyListener(this);
+		frame.setContentPane(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+	
+	//	Timer
+		timer = new Timer(1000/60,this);
+		timer.start();
+	
+	
+	}
+	
+	//	Main Method
+	public static void main (String[] args){
+		new Pong();
+	}
+
+
+
+}
---
+++ PongPanel.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class PongPanel extends JPanel{
+	//Properties
+	int intBallX = 280;
+	int intBallY = 180;
+	
+	boolean blnBallR = true;
+	boolean blnBallL = true;
+	boolean blnBallUp = true;
+	boolean blnBallDown = true;
+	
+	//Padel One
+	int intRect1X = 0;
+	int intRect1Y = 150;
+	
+	boolean blnDown = false;
+	boolean blnUp= false;
+	boolean blnLeft = false;
+	boolean blnRight = false;
+	
+	//Padel Two
+	int intRect2X = 575;
+	int intRect2Y = 150;
+	
+	
+	//Methods
+	public void paintComponent(Graphics g){
+		g.setColor(Color.BLUE);
+		g.fillRect(0,0,600,400);
+		g.setColor(Color.ORANGE);
+		g.fillOval(intBallX,intBallY,20,20);
+		
+		g.setColor(Color.RED);
+		
+		//Rect one
+		g.fillRect(intRect1X,intRect1Y,25,90);
+		
+		//Rect 1
+		if (blnDown){
+			intRect1Y = intRect1Y + 5 ;
+		}else if (blnUp){
+			intRect1Y = intRect1Y - 5 ;
+		}
+		
+		if(intRect1Y < 0){
+			intRect1Y = 0;
+		}else if(intRect1Y > 310){
+			intRect1Y = 310;
+		}
+		
+		
+		//Rect two
+		g.fillRect(intRect2X,intRect2Y,25,90);
+		
+		// RECT 2
+		
+		if(intRect2X > 575){
+			intRect2X = 575;
+		}else if(intRect2Y < 0){
+			intRect2Y = 0;
+		}else if(intRect2Y > 310){
+			intRect2Y = 310;
+		}
+		
+		
+		// Ball	moving Left and Right
+		
+		if (intBallX > (intRect2X)){
+			blnBallR = false;
+			blnBallL = true;
+		}else if(intBallX < (intRect1X)){
+			blnBallL = false;
+			blnBallR = true;
+		}
+		
+		if(blnBallR){
+			intBallX = intBallX + 5;
+		}else if(blnBallL){
+			intBallX = intBallX - 5;
+		}
+		
+		//Ball moving Up and Down
+		if(intBallY > 380){
+			blnBallUp = true;
+			blnBallDown = false;
+		}else if (intBallY < 0){
+			blnBallUp = false;
+			blnBallDown = true;
+		}
+		
+		if(blnBallUp){
+			intBallY = intBallY - 5;
+		}else if(blnBallDown){
+			intBallY = intBallY + 5;
+		}
+		
+	}
+	
+	
+	//Constructor
+	public PongPanel(){
+		
+		
+	}
+
+
+
+
+
+}
